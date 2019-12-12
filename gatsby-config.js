974ef5aaf1e7788ffe82b05513448a16d998d3d4
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-13 00:42:46 +0100

    Edited title meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Black History Month 2020',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
