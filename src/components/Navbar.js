e71ca12d4cd62b8ef2b733768bb91a9d8f812df1
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-12 17:02:26 +0100

    added agenda folder

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Black History Month Belgium" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -62,6 +62,9 @@ const Navbar = class extends React.Component {
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
                 About
+              </Link>
+              <Link className="navbar-item" to="/agenda">
+                Agenda
               </Link>
               <Link className="navbar-item" to="/products">
                 Products
