b86003aa7fa96d22d251cf40089a5ede25ef6254
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-16 20:07:25 +0100

    container for blogs and social media

---
+++ src/components/News.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from 'gatsby'
+import BlogRoll from '../components/BlogRoll'
+const News = () => (
+  <section className="news">
+
+    <div>
+      <h1>Blogs</h1>
+      {/*<BlogRoll />*/}
+    </div>
+
+    <p>News</p>
+  </section>
+)
+export default News
