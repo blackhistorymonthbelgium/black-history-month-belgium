cf65dfc21c32cb0667206729982b8827a8ac46f0
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-16 20:05:44 +0100

    making container for upcoming events

---
+++ src/components/ComingUpEvents.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from 'gatsby'
+const ComingUpEvents = () => (
+  <section className="comingUpEvents">
+    <h1>Coming Up</h1>
+
+    <p>events</p>
+  </section>
+)
+export default ComingUpEvents
