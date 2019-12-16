22e3ee77b19ba2742dc2a86490beca42119ddccc
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-16 21:11:36 +0100

    added map

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -1,10 +1,30 @@
 import React from "react"
 import { Link } from 'gatsby'
+import logo from '../img/bhm-logo.svg'
+const partners = [0,0,0,0,0,0,0,0];
 const Sponsors = () => (
   <section className="sponsors">
-    <h1>Sponsor</h1>
-
-    <p>logo</p>
+    <div className="bePartner">
+      <h1>Our Partners</h1>
+      <p>
+        LOREM IPSUM GENERATOR
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <a href="mailto:blackhistorymonth.belgium@gmail.com?subject=Become BHM Partner&body=Hello BHM! We'd like to be your partner...">Be our partner</a>
+    </div>
+    <ul className="partners">
+      {partners.map((partner, key)=>
+        <li key={key}>
+          <a href="https://www.facebook.com/blackhistorymonthbelgium/">
+            <img
+            src={logo}
+            alt="BHM"
+            style={{height: '3em' }}
+            />
+          </a>
+        </li>
+      )}
+    </ul>
   </section>
 )
 export default Sponsors
