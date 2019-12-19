afdc3191a2ce9fa2a50917d4e3a7217ecbfebe80
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-19 21:46:28 +0100

    changed hmtl comingupevent

--- src/components/ComingUpEvents.js
+++ src/components/ComingUpEvents.js
@@ -9,22 +9,21 @@ const ComingUpEvents = () => (
       {events.map((event, key)=>
       <li key={key} className="column">
         <div className="event-wrapper">
-          <time>
-            <span className="date-event">1-20</span>
-            <span className="month-event">Mar</span>
-          </time>
-          <div className="coming-event">
-          <img
-          src={eventImg}
-          title="Black History Month 2020"
-          alt="Highlight event BHM 2020"
-          />
-          <div className="event-detail">
-          <div className="performancer">
-            <h1>Africa is/in the future 2019</h1>
-            <p>Don Pandzou</p>
-          </div>
-            <div className="detail-btn-event">
+          <div className="event-wrapper-insider">
+            <time>
+              <span className="date-event">1-20</span>
+              <span className="month-event">Mar</span>
+            </time>
+            <div className="performancer">
+              <h1>Africa is/in the future 2019</h1>
+              <p>Don Pandzou</p>
+            </div>
+            <div className="event-detail">
+              <img
+              src={eventImg}
+              title="Black History Month 2020"
+              alt="Highlight event BHM 2020"
+              />
               <div className="detail">
                 <span className="tag-event"> <i className="fal fa-hashtag" />  music debate </span>
                 <span className="location"><i className="fal fa-map-marker-alt"/> Antwerp</span>
@@ -34,7 +33,6 @@ const ComingUpEvents = () => (
                 Detail <i className="far fa-chevron-right"></i>
               </Link>
             </div>
-          </div>
           </div>
         </div>
       </li>
