fd0ae12ec33d9112a2802d207e8e1a6f896d7fb3
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-23 01:03:56 +0100

    deleting products and pricing page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,6 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import ArchivesPostPreview from './preview-templates/ArchivesPostPreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
@@ -13,6 +12,5 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('archives', ArchivesPostPreview)
