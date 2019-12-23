bad37bc84945d94fde1524c54038ac9f66b46a0e
Alfi Yusrina <alfi.yusrina@hotmail.com>
2019-12-23 01:05:16 +0100

    removed old navs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,15 +71,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
-              {/*    <Link className="navbar-item" to="/products">
-                    Products
-                  </Link>
-                  <Link className="navbar-item" to="/blog">
-                    Blog
-                  </Link>
-                  <Link className="navbar-item" to="/contact">
-                    Contact
-                  </Link>*/}
             </div>
           </div>
         </div>
