88db0f65d259469915834557fdc98b26ee8df148
Stan Hebben <development@stanhebben.net>
2020-01-05 12:16:56 +0100

    Add language fields

---
+++ src/locales.js
@@ -0,0 +1,17 @@
+module.exports = {
+  nl: {
+    path: 'nl',
+    locale: 'Dutch',
+    default: true
+  },
+  en: {
+    path: 'en',
+    locale: 'English',
+    default: false
+  },
+  fr: {
+    path: 'fr',
+    locale: 'French',
+    default: false
+  }
+};
