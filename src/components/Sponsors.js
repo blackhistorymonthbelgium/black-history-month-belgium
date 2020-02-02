3d5e459eaa0e09108773b24bd1dad0d174c6142a
Alfi Yusrina <alfi.yusrina@hotmail.com>
2020-02-02 21:48:25 +0100

    Added text for sponssors

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -13,10 +13,15 @@ class Sponsors extends React.Component {
           <div className="columns is-half-desktop is-full-tablet is-full-mobile ">
             <div className="column bePartner">
               <h1>Our Partners</h1>
-              <h2>LOREM IPSUM GENERATOR</h2>
+              <h2>VOOR POTENTIËLE PARTNERS</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </p>
+              Black History Month Belgium streeft inclusie na door, vanuit een sterk gevormde en verankerde kern, samenwerkingen aan te gaan met partners in de culturele, educatieve en sociale sector voor het organiseren van onze evenementen. Via deze partnerschappen hebben we reeds bewezen samen te kunnen werken met partners uit het Vlaams gesubsidieerde cultuur, kunst en onderwijsveld (zie evenementen BHM) en een tal van organisaties uit het hele sociaal-demografische spectrum. Velden die nu nog steeds moeilijkheden ondervinden om onze doelgroep, via hun regulier aanbod, te bereiken.
+              <br/>
+              <br />
+              Samenwerken met organisaties die de streefdoelen van BHM ten harte nemen, speelt een belangrijke rol in het uitbouwen en voortzetten van onze werking. Deze pagina vertegenwoordigt een kleine fractie van de vele organisaties die betrokken zijn bij Black History Month Belgium.
+              <br/>
+              <br />
+              Als je interesse hebt om als organisatie Black History Month Belgium structureel te steunen of als je interesse hebt om als individu, collectief, zwarte organisatie of gemeenschapsorganisatie mee Black History Month inhoudelijk vorm te geven stuur dan een mail met onderstande knop.</p>
                 <a href="mailto:blackhistorymonth.belgium@gmail.com?subject=Become BHM Partner&body=Hello BHM! We'd like to be your partner...">
                   Be our partner
                 </a>
