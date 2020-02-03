5d1d9add3fecb43663ecde67b114894ef04f0f3b
Alfi Yusrina <alfi.yusrina@hotmail.com>
2020-02-03 02:29:37 +0100

    Adding extra partners

--- src/pages/extrapartners/index.js
+++ src/pages/extrapartners/index.js
@@ -8,11 +8,10 @@ export default class ExtraPartnersIndexPage extends React.Component {
       <Layout language={language}>
         <section className="blogs">
           <h1 className="blogs-title">
-            Partners
+            VOOR POTENTIËLE PARTNERS
           </h1>
           <div className="container">
-            <div className="content">
-              <h2>VOOR POTENTIËLE PARTNERS</h2>
+            <div className="content extrapartners">
               <p>
                 Black History Month Belgium streeft inclusie na door, vanuit een sterk gevormde en verankerde kern, samenwerkingen aan te gaan met partners in de culturele, educatieve en sociale sector voor het organiseren van onze evenementen. Via deze partnerschappen hebben we reeds bewezen samen te kunnen werken met partners uit het Vlaams gesubsidieerde cultuur, kunst en onderwijsveld (zie evenementen BHM) en een tal van organisaties uit het hele sociaal-demografische spectrum. Velden die nu nog steeds moeilijkheden ondervinden om onze doelgroep, via hun regulier aanbod, te bereiken.
                 <br/>
@@ -25,6 +24,80 @@ export default class ExtraPartnersIndexPage extends React.Component {
               <a className="emailPartner" href="mailto:blackhistorymonth.belgium@gmail.com?subject=Become BHM Partner&body=Hello BHM! We'd like to be your partner...">
                   Be our partner
               </a>
+              <br/>
+              <br/>
+              <div className="partnersList">
+                <h1 className="blogs-title">2020</h1>
+                <div>
+                  &#8226; AfroYoga
+                  &#8226; Arenberg
+                  &#8226; ASCOM
+                  &#8226; AYO
+                  &#8226; BIB Schaerbeek
+                  &#8226; BINABI
+                  &#8226; Cc Berchem
+                  &#8226; City Pirates
+                  &#8226; De Cinema
+                  &#8226; De Roma
+                  &#8226; FAQ(For All Queens)
+                  &#8226; FoMu?
+                  &#8226; Karibu
+                  &#8226; Kolamela
+                  &#8226; KULeuven
+                  &#8226; Mama's Open Mic
+                  &#8226; Middelheim Museum
+                  &#8226; Rataplan?
+                  &#8226; Recognition
+                  &#8226; Rwanda enz meer
+                  &#8226; Shakuma Fitness
+                  &#8226; MAS
+                  &#8226; UA
+                  &#8226; UMOJA
+                  &#8226; Undivided KUL
+                  &#8226; Urban Woorden
+                  &#8226; VUB-Crosstalks
+                  &#8226; VUB-RHEA
+                  &#8226; Kapow vzw
+                  &#8226; Voem vzw
+                  &#8226; EVA vzw
+                </div>
+              </div>
+              <div className="partnersList">
+                <h1 className="blogs-title">2019</h1>
+                <div>
+                  &#8226; FOMU: Foto Museum
+                  &#8226; DE ROMA
+                  &#8226; ARENBERG
+                  &#8226; KDG SINT-LUCAS
+                  &#8226; FAMEUS
+                  &#8226; CC BERCHEM
+                  &#8226; CINEMA ZUID
+                  &#8226; BIBLIOTHEEK SCHAARBEEK
+                  &#8226; KONINKLIJKE ACADEMIE VOOR SCHONE KUNSTEN
+                  &#8226; CAFé au LAIT
+                  &#8226; DE STUDIO
+                  &#8226; MAMA’S OPEN MIC
+                  &#8226; Het PALEIS
+                  &#8226; Het Bos
+                  &#8226; Permeke
+                  &#8226; Bibliotheek Vilvoorde
+                  &#8226; Kolamela
+                  &#8226; Waka Waka Generation
+                  &#8226; CONGO Travel DRc
+                </div>
+              </div>
+              <div className="partnersList">
+                <h1 className="blogs-title">2018</h1>
+                <div>
+                  &#8226; MAS
+                  &#8226; Universiteit van Antwerpen Departement geschiedenis
+                  &#8226; Universiteit van Antwerpen Cultuur
+                  &#8226; AYO
+                  &#8226; KDG STUVO
+                  &#8226; KDG A-RAAD
+                  &#8226; DE ROMA
+                </div>
+              </div>
             </div>
           </div>
         </section>
