3ce514ac1276f258723ea75e661b5f8f99c11167
Alfi Yusrina <alfi.yusrina@hotmail.com>
2020-02-06 23:30:40 +0100

    sort from the lastest

--- src/templates/agenda-list.js
+++ src/templates/agenda-list.js
@@ -94,7 +94,7 @@ export default ({ pageContext }) => (
     query={graphql`
       query AgendaRollQuery {
         allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___date] }
+          sort: { order: DESC, fields: [frontmatter___datestart] }
           filter: { frontmatter: { templateKey: { eq: "agenda-post" } } }
         ) {
           edges {
