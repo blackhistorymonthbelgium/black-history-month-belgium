b0f7777903416975609d4d265be7d23559198eab
Alfi Yusrina <alfi.yusrina@hotmail.com>
2020-02-10 23:04:42 +0100

    removing img in coming soon component

--- src/components/ComingUpEvents.js
+++ src/components/ComingUpEvents.js
@@ -25,16 +25,6 @@ const ComingUpEvents = (props) => {
                   <p>{event.node.frontmatter.artists.join(", ")}</p>
                 </div>
                 <div className="event-detail">
-                {event.node.frontmatter.featuredimage ? (
-                  <div className="featured-thumbnail" style={{width: 180}}>
-                    <PreviewCompatibleImage
-                      imageInfo={{
-                        image: event.node.frontmatter.featuredimage,
-                        alt: `featured image thumbnail for post ${event.node.frontmatter.title}`,
-                      }}
-                    />
-                  </div>
-                ) : null}
                   <div className="detail">
                     <span className="tag-event"> <i className="fal fa-hashtag" />{event.node.frontmatter.tags.map(tag => (<TagLink key={tag} tag={tag}> {tag} </TagLink>))}</span>
                     <span className="location"><i className="fal fa-map-marker-alt"/> {event.node.frontmatter.location}</span>
