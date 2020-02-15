b34dc160acdbab2a01c12f505a5311c5036b6583
Alfi Yusrina <alfi.yusrina@hotmail.com>
2020-02-16 00:18:25 +0100

    Adding new color based on their tags

--- src/components/AgendaRoll.js
+++ src/components/AgendaRoll.js
@@ -11,21 +11,21 @@ export default class AgendaRoll extends React.Component {
         <ul className=" columns agendaRoll agenda-results">
         {posts &&
           posts.map(({ node: post }) => (
-            <li key={post.id} className={`column is-one-third ${post.frontmatter.featuredpost ? 'is-featured' : ''}`}>
+            <li key={post.id} className={`column is-one-third ${post.frontmatter.featuredpost ? 'is-featured' : ''} ${post.frontmatter.tags ? post.frontmatter.tags.map(tag => "tag-" + tag).join(" ") : ''}`}>
               <div className="event-wrapper-outside">
-                <div className={`event-wrapper ${post.frontmatter.tags ? post.frontmatter.tags.map(tag => "tag-" + tag).join(" ") : 'event-wrapper-default'}`}>
+                <div className={`event-wrapper`}>
                   <div className="event-wrapper-insider">
                     <time>
                       <span className="date-event">{moment(post.frontmatter.datestart).format("DD")}</span>
-                      <span className="month-event">{moment(post.frontmatter.datestart).format("MMM")}</span>
+                      <span className={`month-event`}>{moment(post.frontmatter.datestart).format("MMM")}</span>
                     </time>
                     <div className="performancer">
                       <div className="headerlines"><PostLink post={post}>{post.frontmatter.title}</PostLink></div>
                       <div className="event-detail">
                         <div className="detail">
                           <span className="hostname">{post.frontmatter.artists.join(", ")}</span>
-                          <span className={`tag-event ${post.frontmatter.tags ? post.frontmatter.tags.map(tag => "tag-" + tag).join(" ") : ''}`}> <i className="fal fa-hashtag" />{post.frontmatter.tags.map(tag => (<TagLink key={tag} tag={tag}> {tag} </TagLink>))}</span>
-                          <div className={`highlightloc ${post.frontmatter.tags ? post.frontmatter.tags.map(tag => "tag-" + tag).join(" ") : 'tag-default'}`}>
+                          <span className={`tag-event`}> <i className="fal fa-hashtag" />{post.frontmatter.tags.map(tag => (<TagLink className="tag" key={tag} tag={tag}> {tag} </TagLink>))}</span>
+                          <div className='highlightloc'>
                             <span className="location"><i className="fal fa-map-marker-alt"/> {post.frontmatter.location}</span>
                             <span className="time texttag-game"> <i className="fal fa-clock"/> {moment(post.frontmatter.datestart).format("HH:mm")}</span>
                           </div>
@@ -37,7 +37,7 @@ export default class AgendaRoll extends React.Component {
                 <div className='detail-wrapper'>
                   <div className='detail-spacer' />
                   <div className='detail-link'>
-                    <PostLink post={post}>
+                    <PostLink post={post} className="detail-link">
                       Detail <i className="far fa-chevron-right"></i>
                     </PostLink>
                   </div>
