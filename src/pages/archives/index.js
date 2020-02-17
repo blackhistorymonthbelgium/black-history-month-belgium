2b477433da26a75f23cf064eca6a48d7a4ff16a3
Alfi Yusrina <alfi.yusrina@hotmail.com>
2020-02-17 12:13:09 +0100

    set the language to make sure correct translation

--- src/pages/archives/index.js
+++ src/pages/archives/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import ArchivesRoll from '../../components/ArchivesRoll'
-import {T} from '../../internationalization'
+import {T, setCurrentLanguage} from '../../internationalization'
 
 
 export default class ArchivesIndexPage extends React.Component {
   render() {
     const { language } = this.props.pageContext;
+    setCurrentLanguage(language); // for some reason the setCurrentLanguage in Layout is not enough
     return (
       <Layout language={language}>
         <section className="blogs">
